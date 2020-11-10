1133a34c7802a461a8ea965f27370ff03bcabf79
victorzambrano <ingenieriainversazambrano@gmail.com>
2020-11-10 10:57:52 -0400

    errorr

---
+++ funciones.js
@@ -0,0 +1,14 @@
+// Mensaje por consola
+console.log("Primera Práctica victor adrian");
+// Declarando variables
+var variableVar = "Variable con Var";
+let variableLet = "Variable con Let";
+const variableConst = "Variable con Const";
+// Mostrando variables por consola.
+console.log(12);
+console.log(10);
+console.log(8);
+// Repitiendo declaracion de variable var
+var variableVar = "Variable con Var";
+// Intentando asignar nuevo valor a variableConst
+variableConst = "Nuevo Valor de Variable Const";
